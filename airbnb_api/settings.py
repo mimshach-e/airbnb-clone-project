9d592b3c886c7a57dee6dca16d73178c949e5d5f
Ernest Mimshach <mimshach.e@gmail.com>
2025-05-03 17:24:30 +0000

    made changes to file

--- airbnb_api/settings.py
+++ airbnb_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'django_filters',
     'account',
     'property',
     'booking',
